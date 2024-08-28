8b4acd5b307d7d29def50b1bf38f2b2699b8c651
tm-wayne-dayata <wayne@thinkingmachin.es>
2024-08-28 17:35:18 +0800

    fix: cast to int

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser.add_argument("-a", "--num_a", help="value of num_a")
 parser.add_argument("-b", "--num_b", help="value of num_b")
 args = vars(parser.parse_args())
 
-num_a = args['num_a']
-num_b = args['num_b']
+num_a = int(args['num_a'])
+num_b = int(args['num_b'])
 
 print_values(num_a, num_b)
